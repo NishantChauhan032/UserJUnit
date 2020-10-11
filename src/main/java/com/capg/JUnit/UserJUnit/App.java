dc5a79f98b67cbb11109b11625c94b006aa5ce19
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-11 18:02:21 +0530

    [NISHANT] refactor : Lambda Expression Used for User input validation

--- src/main/java/com/capg/JUnit/UserJUnit/App.java
+++ src/main/java/com/capg/JUnit/UserJUnit/App.java
@@ -5,27 +5,21 @@ import java.util.regex.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@FunctionalInterface
+interface IUserInputValidation{
+	boolean validate(String userInput);
+}
+
 public class App {
 
 	private static Logger LOG = LogManager.getLogger(App.class);
 	static Scanner sc = new Scanner(System.in);
-
-	public boolean validateName(String name) {
-		boolean validate = Pattern.matches("[A-Z]{1}[a-z]{2,}", name);
-		return validate;
-	}
-	public boolean validateEmail(String email) {
-		boolean validate = Pattern.matches("^[A-Za-z0-9]+([.+-][A-Za-z0-9-]+)?@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)?(\\.[A-Za-z]{2,})$", email);
-		return validate;
-	}
-	public boolean validateMobile(String mobile) {
-		boolean validate = Pattern.matches("^[0-9]{2}[ ][0-9]{10}$", mobile);
-		return validate;
-	}
-	public boolean validatePassword(String password) {
-		boolean validate = Pattern.matches("(?=^.{8,}$)(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]*([^(A-Za-z0-9)]{1})[0-9a-zA-Z]*", password);
-		return validate;
-	}
+	
+	IUserInputValidation validateName = name -> name.matches("[A-Z]{1}[a-z]{2,}");
+	IUserInputValidation validateEmail = email -> email.matches("^[A-Za-z0-9]+([.+-][A-Za-z0-9-]+)?@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)?(\\.[A-Za-z]{2,})$");
+	IUserInputValidation validateMobile = mobile -> mobile.matches("^[0-9]{2}[ ][0-9]{10}$");
+    IUserInputValidation validatePassword = password -> password.matches("(?=^.{8,}$)(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]*([^(A-Za-z0-9)]{1})[0-9a-zA-Z]*");
+	
 
 	public static void main(String[] args) {
 		App newUser = new App();
@@ -33,7 +27,7 @@ public class App {
 		LOG.info("Enter the First Name:");
 		String firstName = sc.next();
 		try {
-		if(!(newUser.validateName(firstName))) {
+		if(!(newUser.validateName.validate(firstName))) {
 			throw new InvalidInputException("Invalid First Name");
 		}}
 		catch(InvalidInputException e){
@@ -43,7 +37,7 @@ public class App {
 		LOG.info("Enter the Last Name:");
 		String lastName = sc.next();
 		try {
-		if(!(newUser.validateName(lastName))) {
+		if(!(newUser.validateName.validate(lastName))) {
 			throw new InvalidInputException("Invalid Last Name");
 		}}
 		catch(InvalidInputException e) {
@@ -53,7 +47,7 @@ public class App {
 		LOG.info("Enter the Email:");
 		String email = sc.next();
 		try {
-		if(!(newUser.validateEmail(email))) {
+		if(!(newUser.validateEmail.validate(email))) {
 			throw new InvalidInputException("Invalid Email Id");
 		}}
 		catch(InvalidInputException e) {
@@ -64,7 +58,7 @@ public class App {
 		LOG.info("Enter the Mobile Number:");
 		String mobile = sc.nextLine();
 		try {
-		if (!(newUser.validateMobile(mobile))) {
+		if (!(newUser.validateMobile.validate(mobile))) {
 			throw new InvalidInputException("Invalid Email Id");
 		}}
 		catch(InvalidInputException e) {
@@ -74,7 +68,7 @@ public class App {
 		LOG.info("Enter the Password:");
 		String password = sc.next();
 		try {
-		if (!(newUser.validateMobile(password))) {
+		if (!(newUser.validatePassword.validate(password))) {
 			throw new InvalidInputException("Invalid Password");
 		}}
 		catch(InvalidInputException e) {
